43c0ead46f0b1f2aca2ebc2754e9c7190fdaab34
Adrijan <adrijangithub@gmail.com>
2025-09-26 21:35:08 +0200

    Add Home component for BaseCamp dApp template

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+// frontend/app/page.js
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to BaseCamp!</h1>
+      <p>This is your beginner-friendly Base dApp template.</p>
+    </div>
+  );
+}
